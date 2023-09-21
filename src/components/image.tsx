e9da1e71c9982731d872a68e098f15d5de92a46d
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-09-21 19:26:34 +0500

    image optimisation

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -18,9 +18,9 @@ const CustomImage: FC<Props> = ({ product, fill }) => {
             {fill ? (
                 <Image src={product.image} alt={product.title} fill className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${isLoading ? "scale-110 blur-2xl grayscale" : "scale-100 blur-0 grayscale-0"}`} onLoadingComplete={() => setIsLoading(false)} />
             ) : (
-                <Image src={product.image} alt={product.title} 
-                width={400}
-                fill className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${isLoading ? "scale-110 blur-2xl grayscale" : "scale-100 blur-0 grayscale-0"}`} />
+                <Image src={product.image} alt={product.title}
+                    width={400}
+                    fill className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${isLoading ? "scale-110 blur-2xl grayscale" : "scale-100 blur-0 grayscale-0"}`} onLoadingComplete={() => setIsLoading(false)} />
             )}
         </>
     )
