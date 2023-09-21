fb0ece07b277548e54d917fab9b89dc3e3c4d306
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-09-21 18:21:05 +0500

    server rendering component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
+import Hero from '@/components/hero';
+import Product from '@/components/product';
+import { ProductType } from '@/interfaces';
 import React from 'react'
 
-const HomePage = () => {
+const HomePage = async () => {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const products: ProductType[] = await res.json();
+
+
+
   return (
-    <div>HomePage</div>
+    <main className="min-h-screen max-w-7xl mx-auto px-8 xl:px-0">
+      <Hero />
+      <section className="flex flex-col space-y-12">
+        <h1 className="text-5xl font-bold text-center">Next Shop Deals</h1>
+        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <Product key={product.id} product={product} />
+          ))}
+        </div>
+      </section>
+    </main>
   )
 }
 
