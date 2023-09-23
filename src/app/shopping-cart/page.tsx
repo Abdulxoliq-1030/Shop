fc6118ce6f1bcbf33f1d6ad66480055065d3908b
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-09-23 12:54:46 +0500

    complete project

--- src/app/shopping-cart/page.tsx
+++ src/app/shopping-cart/page.tsx
@@ -211,7 +211,7 @@ const ShoppingCart = () => {
                             Shopping cart is empty
                         </h1>
                         <p className='text-slate-600 mt-5 lg:text-lg'>
-                            The page you are looking for doesn't exist or <br />
+                            The page you are looking for does not exist or <br />
                             has been removed.
                         </p>
                         <Link href={'/products'}>
--- src/components/cta.tsx
+++ src/components/cta.tsx
@@ -22,7 +22,7 @@ const Cta: React.FC<CtaProps> = () => {
                         <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                     </div>
                     <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-                    <p className="text-xs text-gray-500 mt-3">Literally you probably haven't heard of them jean shorts.</p>
+                    <p className="text-xs text-gray-500 mt-3">Literally you probably have not heard of them jean shorts.</p>
                 </div>
             </div>
         </section>
